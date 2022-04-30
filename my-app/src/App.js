16b9aaf399505c52cd7a5e428e4448b34487a8c8
antarpreet11 <singhantarpreet11@gmail.com>
2022-04-30 03:18:38 -0700

    Added dummy list of words and used them

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,39 @@
-import UserForm from './components/Form/UserForm';
 import React, { useState } from 'react';
+import UserForm from './components/Form/UserForm';
+import FlashCardList from './components/FlashCard/FlashCardList';
 
+const dummyWords = [
+  {
+    lang1: 'Hello',
+    lang2: 'Salut'
+  },
+  {
+    lang1: 'Here',
+    lang2: 'Ici'
+  },
+  {
+    lang1: 'Sorry',
+    lang2: 'Pardon'
+  },
+  {
+    lang1: 'No',
+    lang2: 'Non'
+  },
+  {
+    lang1: 'When',
+    lang2: 'Quand'
+  }
+];
 
 function App() {
+
+  const [words, setWords] = useState(dummyWords);
+
   return (
-    <UserForm></UserForm>
+    <div className='App'>
+      <UserForm></UserForm>
+      <FlashCardList words={words}></FlashCardList>
+    </div>
   );
 }
 
